2de91edef9cda37e84781c98434b7eb3894654e0
evangryska94 <eagryska@gmail.com>
2017-12-06 11:24:49 -0600

    Adds tests for dashboard component

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {DebugElement} from '@angular/core';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Observable } from 'rxjs/Rx';
 
@@ -10,6 +11,7 @@ import { HeroSearchComponent } from '../hero-search/hero-search.component';
 
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 import { MessageService } from '../message.service';
 
 describe('DashboardComponent', () => {
@@ -20,7 +22,6 @@ describe('DashboardComponent', () => {
   let rootEl:      HTMLElement;
   let heroService;
   let spy;
-  let heroes: Hero[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -38,21 +39,52 @@ describe('DashboardComponent', () => {
     rootEl = fixture.debugElement.nativeElement;
     heroService = TestBed.get(HeroService);
 
-    let hero1 = new Hero(1, "hero1");
-    let hero2 = new Hero(2, "hero2");
-    let hero3 = new Hero(3, "hero3");
-    heroes = [hero1, hero2, hero3];
-
     //spy on getHeroes
     spy = spyOn(heroService, 'getHeroes')
-      .and.returnValue(Observable.of(heroes));
+      .and.returnValue(Observable.of(HEROES));
 
     //assign debug element with css selector
+    de = fixture.debugElement.query(By.css('.grid'));
+    el = de.nativeElement;
+  });
 
-    fixture.detectChanges();
+  it('should not call getHeroes before onInit', () => {
+    expect(el.innerText).toBe('', 'nothing displayed');
+    expect(spy.calls.any()).toBe(false, 'getHeroes not yet called');
   });
 
   it('should be created', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should call getHeroes and populate heroes array', () => {
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    let foundHeroes = component.heroes;
+
+    expect(foundHeroes.length).toEqual(4);
+    expect(foundHeroes).toEqual(HEROES.slice(1,5));
+  });
+
+  it('should populate heroes grid on dashboard with mock data', () => {
+    fixture.detectChanges();
+    let pageData =  el.innerHTML;
+    let displayedHeroes = fixture.debugElement.queryAll(By.css('.module.hero'));
+
+    expect(displayedHeroes.length).toEqual(4);
+
+    expect(pageData).toContain('ng-reflect-router-link="/detail/12"');
+    expect(displayedHeroes[0].nativeElement.innerText).toContain('Narco');
+
+    expect(pageData).toContain('ng-reflect-router-link="/detail/13"');
+    expect(displayedHeroes[1].nativeElement.innerText).toContain('Bombasto');
+
+    expect(pageData).toContain('ng-reflect-router-link="/detail/14"');
+    expect(displayedHeroes[2].nativeElement.innerText).toContain('Celeritas');
+
+    expect(pageData).toContain('ng-reflect-router-link="/detail/15"');
+    expect(displayedHeroes[3].nativeElement.innerText).toContain('Magneta');
   });
 });
