eb565780f121f09cc510ddaa4be525b4c629a80f
evangryska94 <eagryska@gmail.com>
2017-12-06 10:08:18 -0600

    Adds scaffolding for more extensive component testing in dashboard

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,30 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Rx';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { DashboardComponent } from './dashboard.component';
 import { HeroSearchComponent } from '../hero-search/hero-search.component';
 
 import { HeroService } from '../hero.service';
-import { Observable } from 'rxjs/Rx';
+import { Hero } from '../hero';
+import { MessageService } from '../message.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
-
-  class HeroServiceMock {
-    //spy on functions and stuffs
-    getHeroes (): Observable<Object[]> {
-        return Observable.of([new Object]);
-    }
-  }
+  let de:      DebugElement;
+  let el:      HTMLElement;
+  let rootEl:      HTMLElement;
+  let heroService;
+  let spy;
+  let heroes: Hero[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DashboardComponent, HeroSearchComponent ],
-      imports: [RouterTestingModule],
-      providers: [
-          {provide: HeroService, useClass: HeroServiceMock}
-      ]
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [ HeroService, MessageService ]
     })
     .compileComponents();
   }));
@@ -32,6 +34,21 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+
+    rootEl = fixture.debugElement.nativeElement;
+    heroService = TestBed.get(HeroService);
+
+    let hero1 = new Hero(1, "hero1");
+    let hero2 = new Hero(2, "hero2");
+    let hero3 = new Hero(3, "hero3");
+    heroes = [hero1, hero2, hero3];
+
+    //spy on getHeroes
+    spy = spyOn(heroService, 'getHeroes')
+      .and.returnValue(Observable.of(heroes));
+
+    //assign debug element with css selector
+
     fixture.detectChanges();
   });
 
