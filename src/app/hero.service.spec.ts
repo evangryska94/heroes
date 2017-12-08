774b102cb2c0cb33427790f263b5589cc5d29d8f
evangryska94 <eagryska@gmail.com>
2017-12-08 12:59:02 -0600

    Adds mocked http tests for getHeroes in service
      + Fixes test setup to follow Angular4 http testing standards

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,6 +1,7 @@
 import { async, inject, TestBed } from '@angular/core/testing';
 import { MockBackend, MockConnection } from '@angular/http/testing';
-import { HttpClientModule, HttpClient, HttpXhrBackend, HttpResponse } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpResponse, HttpRequest } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -15,10 +16,8 @@ describe('HeroService', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [ HttpClientModule ],
-            providers: [ HeroService, MessageService,
-              { provide: HttpXhrBackend, useClass: MockBackend }
-            ]
+            imports: [ HttpClientModule, HttpClientTestingModule ],
+            providers: [ HeroService, MessageService ]
           }).compileComponents();
     }));
 
@@ -36,7 +35,53 @@ describe('HeroService', () => {
         expect(service instanceof HeroService).toBe(true);
     }));
 
-    it('should provide the MockBackend as the injected HttpXhrBackend', inject([HttpXhrBackend], (backend: MockBackend) => {
-        expect(backend).not.toBeNull('backend should be provided');
-    }));
+    describe('GET requests', () => {
+        let backend: HttpTestingController;
+        let heroService: HeroService;
+        let response;
+        let spy;
+
+        beforeEach(inject([HttpClient, HttpTestingController], (httpClient: HttpClient, mockBackend: HttpTestingController) => {
+            backend = mockBackend;
+            heroService = new HeroService(httpClient, messageService);
+
+            spy = spyOn(messageService, 'add').and.stub();
+        }));
+
+        afterEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+            backend.verify();
+        }));
+
+        it('should send request to get all heroes', async() => {
+            heroService.getHeroes().subscribe();
+
+            backend.expectOne((request: HttpRequest<any>) => {
+                return request.url === 'api/heroes'
+                    && request.method === 'GET';
+            });
+        });
+
+        it('should return all heroes retrieved', async() => {
+            heroService.getHeroes().subscribe((result) => {
+                expect(result.length).toBe(HEROES.length);
+                expect(result).toBe(HEROES);
+                expect(spy).toHaveBeenCalledTimes(1);
+                expect(spy).toHaveBeenCalledWith('HeroService: fetched heroes');
+            });
+
+            backend.expectOne('api/heroes').flush(HEROES)
+        });
+
+        it('should handle error', async() => {
+            heroService.getHeroes().subscribe((result) => {
+                console.log(result);
+                expect(result).toEqual([]);
+                expect(spy).toHaveBeenCalledTimes(1);
+                expect(spy).toHaveBeenCalledWith('HeroService: getHeroes failed: Http failure response for api/heroes: 0 ');
+            });
+
+            backend.expectOne('api/heroes').error(new ErrorEvent('test error'));
+        });
+
+    });
 });
